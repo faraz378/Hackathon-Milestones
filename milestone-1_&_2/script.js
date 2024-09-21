a6d74856054586d20c3ee708efdf3d55d1f01fbb
Muhammad Faraz Gul <162604528+faraz378@users.noreply.github.com>
2024-09-21 17:38:24 +0500

    Add files via upload

---
+++ milestone-1_&_2/script.js
@@ -0,0 +1,11 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+var toggleButton = document.getElementById('toggle-skills');
+var skills = document.getElementById('skills');
+toggleButton.addEventListener('click', function () {
+    if (skills.style.display === 'none') {
+        skills.style.display = 'block';
+    }
+    else {
+        skills.style.display = 'none';
+    }
+});
